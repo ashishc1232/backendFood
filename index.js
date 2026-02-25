9dc4d33a8b9e154ea9c407a164abf694b464f260
Ashish <ashishwork66@gmail.com>
2026-02-25 22:30:14 +0530

    updated with bind

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ app.use((err,req,res,next)=>{
 
 const PORT=process.env.PORT || 5000
 
-app.listen(PORT,()=>{
-    console.log(`server successfully running on http://localhost:${PORT}`)
-})
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
