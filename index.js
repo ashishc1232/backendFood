81a1bdca4179360f13479bc4f0427559cb3eb4c4
Ashish <ashishwork66@gmail.com>
2026-02-27 12:03:42 +0530

    updatec cors

--- index.js
+++ index.js
@@ -4,16 +4,29 @@ const restaurantRoutes = require("./routes/restaurant.routes.js");
 const orderRoutes = require("./routes/order.Routes.js");
 const paymentRoutes = require("./routes/payment.Route.js");
 const menuRoutes = require("./routes/menu.Routes.js");
+const rateLimit=require("express-rate-limit")
 const cors=require("cors")
 const app=express();
-require("dotenv").config()
+require("dotenv").config();
 require("./config/db.js");
 
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
+    origin:[
+      "http://localhost:5173",
+      "https://verdant-jelly-b89609.netlify.app"
+    ]
 }))
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Max 100 requests per IP per windowMs
+  standardHeaders: 'draft-7', // Enable standard RateLimit headers (IETF)
+  legacyHeaders: false, // Disable legacy X-RateLimit-* headers
+  message: "Too many requests, please try again later." // Custom message
+});
+
+// Apply the rate limiting middleware to all requests
+app.use(limiter);
 app.use(express.json())
 app.get("/",(req,res)=>{
 res.send("Hello")
